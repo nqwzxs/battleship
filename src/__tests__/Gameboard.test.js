d6716908338dd03679579cf3589ef1a2417c48d9
nqwzxs <127120589+nqwzxs@users.noreply.github.com>
2023-08-24 14:39:20 +0500

    Add more tests to Gameboard class

--- src/__tests__/Gameboard.test.js
+++ src/__tests__/Gameboard.test.js
@@ -14,10 +14,7 @@ describe("selectSquare()", () => {
 
     const selectedSquare = gameboard.selectSquare(row, col);
 
-    const expectedSquare = gameboard.squares.find(
-      (square) =>
-        square.coordinates.row === row && square.coordinates.col === col,
-    );
+    const expectedSquare = gameboard.squares[99];
 
     expect(selectedSquare).toEqual(expectedSquare);
   });
@@ -71,6 +68,40 @@ describe("selectSquares()", () => {
 
     expect(selectedSquares).toEqual(expectedSquares);
   });
+
+  test("should return null if ship is out of bounds", () => {
+    const shipLength = 7;
+    const row = 7;
+    const col = 7;
+    const vertical = false;
+
+    const selectedSquares = gameboard.selectSquares(
+      shipLength,
+      row,
+      col,
+      vertical,
+    );
+
+    expect(selectedSquares).toBe(null);
+  });
+
+  test("should return null if one of selected squares is busy", () => {
+    const shipLength = 5;
+    const row = 0;
+    const col = 0;
+    const vertical = false;
+
+    gameboard.squares[0].busy = true;
+
+    const selectedSquares = gameboard.selectSquares(
+      shipLength,
+      row,
+      col,
+      vertical,
+    );
+
+    expect(selectedSquares).toBe(null);
+  });
 });
 
 describe("placeShip()", () => {
@@ -78,25 +109,87 @@ describe("placeShip()", () => {
     const ship = new Ship(3);
     const row = 1;
     const column = 1;
+    const vertical = false;
 
-    const selectedSquare = gameboard.selectSquare(row, column);
+    const selectedSquares = gameboard.selectSquares(
+      ship.length,
+      row,
+      column,
+      vertical,
+    );
 
-    Gameboard.placeShip(ship, selectedSquare);
+    gameboard.placeShip(ship, selectedSquares);
 
-    expect(selectedSquare.ship).toBe(ship);
+    selectedSquares.forEach((square) => {
+      expect(square.ship).toBe(ship);
+    });
+  });
+
+  test("should set ship's adjacent squares busy", () => {
+    const ship = new Ship(1);
+    const row = 1;
+    const column = 1;
+    const vertical = false;
+
+    const selectedSquares = gameboard.selectSquares(
+      ship.length,
+      row,
+      column,
+      vertical,
+    );
+
+    const adjacentSquareIndexes = [0, 1, 2];
+
+    gameboard.placeShip(ship, selectedSquares);
+
+    adjacentSquareIndexes.forEach((index) => {
+      expect(gameboard.squares[index].busy).toBe(true);
+    });
   });
 });
 
-describe("placeShip()", () => {
-  test("should place ship on specified squares", () => {
-    const ship = new Ship(3);
-    const row = 0;
-    const column = 0;
+describe("allSunk()", () => {
+  test("should return true if all ships are sunk", () => {
+    Object.values(gameboard.ships).forEach((ship) => {
+      ship.sunken = true;
+    });
 
-    const selectedSquare = gameboard.selectSquare(row, column);
+    expect(gameboard.allSunk()).toBe(true);
+  });
 
-    Gameboard.placeShip(ship, selectedSquare);
+  test("should return false if not all ships are sunk", () => {
+    expect(gameboard.allSunk()).toBe(false);
+  });
+});
 
-    expect(selectedSquare.ship).toBe(ship);
+describe("receiveAttack()", () => {
+  test("should set square shot", () => {
+    const square = gameboard.squares[0];
+
+    gameboard.receiveAttack(square);
+
+    expect(square.shot).toBe(true);
+  });
+
+  test("should call ship.hit() if ship is hit", () => {
+    const square = gameboard.squares[0];
+    square.ship = new Ship(3);
+
+    gameboard.receiveAttack(square);
+
+    expect(square.ship.hits).toEqual(1);
+  });
+
+  test("should shoot adjacent squares if ship is sunk", () => {
+    const square = gameboard.squares[11];
+    square.ship = new Ship(1);
+
+    gameboard.receiveAttack(square);
+
+    const adjacentSquareIndexes = [0, 1, 2, 10, 12, 20, 21, 22];
+
+    adjacentSquareIndexes.forEach((index) => {
+      expect(gameboard.squares[index].shot).toBe(true);
+    });
   });
 });
