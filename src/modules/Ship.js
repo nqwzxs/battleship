ef12723b4206778d6b9af79513420523e849e04d
nqwzxs <127120589+nqwzxs@users.noreply.github.com>
2023-08-24 14:58:34 +0500

    Add isSunk function to hit function in Ship class

--- src/modules/Ship.js
+++ src/modules/Ship.js
@@ -2,11 +2,12 @@ export default class Ship {
   constructor(length) {
     this.length = length;
     this.hits = 0;
-    this.sunken = this.isSunk();
+    this.sunken = false;
   }
 
   hit() {
     this.hits += 1;
+    this.sunken = this.isSunk();
   }
 
   isSunk() {
